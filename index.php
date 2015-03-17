ff93635fe0ddb8fd03e4fa775aa8fbde41f4dea9
jeanyves-yang <jean-yves.yang@cpe.fr>
2015-03-17 18:28:40 -0400

    NEW: add files necessary for bootstrap and javascript/jquery use.

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 <html>
 	<head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
-	<title>Particles &#92;&#111;&#47;</title>
+	<title>Particles</title>
+
+	<script type="text/javascript" src="scripts/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="js/jquery-ui-1.10.4.custom.min.js"></script>
+
+	<script type="text/javascript" src="scripts/stats.js"></script>
 	</head>
 
 	<body>
