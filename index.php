207e8d51ce945f707f8e1d457a6016c81a95fa9c
jeanyves-yang <jean-yves.yang@cpe.fr>
2015-03-17 16:15:30 -0400

    NEW: Index page initialized.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+	<title>Particles &#92;&#111;&#47;</title>
+	</head>
+
+	<body>
+
+	</body>
+<html>
